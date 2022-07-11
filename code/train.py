18ee1d414b90fcae7a2b230dfe2d7d0206221bce
Annan-NHT <annan@nighthack.in>
2022-07-11 22:28:37 +0530

    first commit

---
+++ code/train.py
@@ -0,0 +1,46 @@
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from model import load_model
+import numpy as np 
+import argparse
+
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('n_epochs',type=int)
+
+args=parser.parse_args()
+
+X_train=np.load('AD.npy')
+frames=X_train.shape[2]
+#Need to make number of frames divisible by 10
+
+
+frames=frames-frames%10
+
+X_train=X_train[:,:,:frames]
+X_train=X_train.reshape(-1,227,227,10)
+X_train=np.expand_dims(X_train,axis=4)
+Y_train=X_train.copy()
+
+
+
+epochs=args.n_epochs
+batch_size=1
+
+
+
+if __name__=="__main__":
+
+	model=load_model()
+
+	callback_save = ModelCheckpoint("Simply.h5",monitor='val_loss', verbose=1,save_best_only=True,mode='auto')
+
+	callback_early_stopping = EarlyStopping(monitor='val_loss', patience=3,verbose=1, mode='auto') #avoid overfit
+
+	print('Model has been loaded')
+
+	model.fit(X_train,Y_train,validation_split=0.3,batch_size=batch_size,epochs=epochs,callbacks = [callback_save,callback_early_stopping])
+
+	model.save('AD.h5')
+
+
